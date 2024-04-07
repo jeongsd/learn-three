6c357251dfc4c454678c1bdd5f979cd6d3d0d507
jeongsd <asdfg852456@gmail.com>
2024-04-08 07:02:45 +0900

    material

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,38 +2,43 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <ul>
-      <li>
-        <Link href="/lessons/first-threejs-project">
-          03. lessons/first-threejs-project
-        </Link>
-      </li>
-      <li>
-        <Link href="/lessons/transform-objects">04. transform-objects</Link>
-      </li>
-      <li>
-        <Link href="/lessons/animation">05. Animation</Link>
-      </li>
-      <li>
-        <Link href="/lessons/camera">06. Camera</Link>
-      </li>
-      <li>
-        <Link href="/lessons/fullscreen-and-resizing">
-          07. Fullscreen and resizing
-        </Link>
-      </li>
-      <li>
-        <Link href="/lessons/vector">08. vector</Link>
-      </li>
-      <li>
-        <Link href="/lessons/geometry">09. geometry</Link>
-      </li>
-      <li>
-        <Link href="/lessons/debug-ui">debug-ui</Link>
-      </li>
-      <li>
-        <Link href="/lessons/textures">10. textures</Link>
-      </li>
-    </ul>
+    <div className="body">
+      <ul>
+        <li>
+          <Link href="/lessons/first-threejs-project">
+            03. lessons/first-threejs-project
+          </Link>
+        </li>
+        <li>
+          <Link href="/lessons/transform-objects">04. transform-objects</Link>
+        </li>
+        <li>
+          <Link href="/lessons/animation">05. Animation</Link>
+        </li>
+        <li>
+          <Link href="/lessons/camera">06. Camera</Link>
+        </li>
+        <li>
+          <Link href="/lessons/fullscreen-and-resizing">
+            07. Fullscreen and resizing
+          </Link>
+        </li>
+        <li>
+          <Link href="/lessons/vector">08. vector</Link>
+        </li>
+        <li>
+          <Link href="/lessons/geometry">09. geometry</Link>
+        </li>
+        <li>
+          <Link href="/lessons/debug-ui">debug-ui</Link>
+        </li>
+        <li>
+          <Link href="/lessons/textures">10. textures</Link>
+        </li>
+        <li>
+          <Link href="/lessons/material">11. material</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
