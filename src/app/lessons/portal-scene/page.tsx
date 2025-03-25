833b232f47669bb5327c3af82a19a6dcd393abc7
Jeong Seong Dae <asdfg852456@gmail.com>
2025-03-25 16:40:13 +0900

    포탈씬 추가

---
+++ src/app/lessons/portal-scene/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import "./style.css";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience";
+
+
+function Page() {
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [- 4, 3, 6]
+        }}
+        flat
+      >
+        <Experience />
+      </Canvas>
+    </div>
+  );
+}
+
+export default Page;
